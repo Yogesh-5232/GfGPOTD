3fd3c479884b04053155eac7ac39c0c28ad35256
G Yogesh <116435014+Yogesh-5232@users.noreply.github.com>
2023-08-22 22:28:02 +0530

    Create Make Matrix Beautiful.c
    A beautiful matrix is a matrix in which the sum of elements in each row and column is equal. Given a square matrix of size NxN. Find the minimum number of operation(s) that are required to make the matrix beautiful. In one operation you can increment the value of any one cell by 1.
    Example 1:
    
    Input:
    N = 2
    matrix[][] = {{1, 2},
                  {3, 4}}
    Output: 
    4
    Explanation:
    Updated matrix:
    4 3
    3 4
    1. Increment value of cell(0, 0) by 3
    2. Increment value of cell(0, 1) by 1
    Hence total 4 operation are required.

---
+++ Make Matrix Beautiful.c
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int> > matrix, int n)
+    {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        int maxi=0;
+        vector<int> allrowsums;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            int sum1=0;
+            for(int j=0;j<n;j++){
+                sum+=matrix[i][j];
+                sum1+=matrix[j][i];
+            }
+            allrowsums.push_back(sum);
+            maxi=max(maxi,max(sum,sum1));
+        }
+        
+        int result=0;
+        for(int i=0;i<n;i++){
+            result+=(maxi-allrowsums[i]);
+        }
+        return result;
+    } 
+};
